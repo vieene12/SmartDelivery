599783c035483a66bdd352785e69fe1e0c007652
vieene12 <maivy0822@gmail.com>
2026-05-29 15:46:39 +0700

    Add pending migration for KhoHang TrangThai

---
+++ Data/Migrations/20260529084416_AutoMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SDMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrangThai",
+                table: "KhoHang",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrangThai",
+                table: "KhoHang");
+        }
+    }
+}
